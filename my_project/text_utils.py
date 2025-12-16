3cc5c0e6e408604574dfdd7b02dcdc0466cde171
alexandra <vngrdvalexandra@yandex.ru>
2025-12-16 19:03:43 +0300

    Update text_utils.py

--- my_project/text_utils.py
+++ my_project/text_utils.py
@@ -1,20 +1,4 @@
-def read_text_file(filepath):
-    """
-    Читает содержимое текстового файла.
 
-    Args:
-        filepath (str): Путь к файлу
-
-    Returns:
-        str: Содержимое файла или сообщение об ошибке
-    """
-    # Ваш код здесь
-    try:
-        with open(filepath,"r", encoding="utf-8") as f:
-            content = f.read()
-    except FileNotFoundError:
-        return "Ошибка: Файл не найден"
-    return content
 
 def count_words(text):
     """
