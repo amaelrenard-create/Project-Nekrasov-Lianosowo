82c2440adfd8a82740561090e3fdadde63269497
alexandra <vngrdvalexandra@yandex.ru>
2025-12-18 19:00:02 +0300

    Update text_utils.py

--- my_project/text_utils.py
+++ my_project/text_utils.py
@@ -1,8 +1,7 @@
 
-
 def count_words(text):
     """
-    Подсчитывает количество слов в тексте.
+    Подсчитывает количество слов в тексте
 
     Args:
         text (str): Текст для анализа
@@ -15,10 +14,28 @@ def count_words(text):
     return count
     
 def count_unique_words(text):
+    """
+    Подсчитывает количество уникальных слов в тексте
+
+    Args:
+        text (str): Входной текст
+
+    Returns:
+        int: Количество уникальных слов
+    """
     words = text.lower().split()
     return len(set(words))
 
 def calculate_ttr(text):
+    """
+    Вычисляет TTR = количество уникальных слов / общее количество слов
+
+    Args:
+        text (str): Входной текст
+
+    Returns:
+        float: TTR текста (от 0 до 1). Если слов нет, возвращает 0
+    """
     words = text.lower().split()
     
     if not words:
@@ -28,6 +45,17 @@ def calculate_ttr(text):
     return len(unique_words) / len(words)
 
 def get_most_common_words(text, n=10):
+     """
+    Находит n самых часто встречающихся слов
+
+    Args:
+        text (str): Входной текст.
+        n (int): Количество самых частых слов для возврата (10)
+
+    Returns:
+        list of tuples: Список кортежей вида (слово, количество), 
+                        отсортированных по убыванию частоты
+    """
     words = text.lower().split()
     result = {}
     
@@ -37,9 +65,27 @@ def get_most_common_words(text, n=10):
     return sorted(result.items(), key=lambda x: x[1], reverse=True)[:n]
 
 def count_lines(text):
+    """
+    Подсчитывает количество строк в тексте.
+
+    Args:
+        text (str): Входной текст
+
+    Returns:
+        int: Количество строк (включая пустые)
+    """
     return len(text.splitlines())
 
 def average_word_length(text):
+     """
+    Вычисляет среднюю длину слова в тексте.
+
+    Args:
+        text (str): Входной текст.
+
+    Returns:
+        float: Средняя длина слова. Если слов нет, возвращает 0.
+    """
     words = text.split()
     
     if not words:
